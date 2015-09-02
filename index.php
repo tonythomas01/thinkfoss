4c2a21b536899dde78f9e3495086ea9bdf65efd9
tonythomas01 <01tonythomas@gmail.com>
2015-09-02 22:53:09 +0530

    Welcome to ThinkFOSS

--- index.php
+++ index.php
@@ -137,7 +137,7 @@
     <div id="tf-home" class="text-center">
         <div class="overlay">
             <div class="content">
-                <h1>Welcome on <strong>Think<span class="color">FOSS</span></strong></h1>
+                <h1>Welcome to <strong>Think<span class="color">FOSS</span></strong></h1>
                 <p class="lead"><strong>< code | train | grow ></strong></p>
                 </div>
             <br>
