a2c830f4832a92db9b60d14eb878bed2bf91b030
tonythomas01 <01tonythomas@gmail.com>
2015-10-06 22:00:49 +0530

    fix google oauth stuff

--- assets/php/oauth/oauth2callback.php
+++ assets/php/oauth/oauth2callback.php
@@ -6,7 +6,7 @@ session_start();
 
 $client = new Google_Client();
 $client->setAuthConfigFile('../access/client_secret.json');
-$client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/php/oauth/oauth2callback.php');
+$client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/assets/php/oauth/oauth2callback.php');
 $client->addScope( Google_Service_Plus::USERINFO_EMAIL );
 $client->addScope( Google_Service_Plus::PLUS_ME );
 
