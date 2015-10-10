c20cb4f4ba1609b1833bff88cdcd9b85b7689cfe
tonythomas01 <01tonythomas@gmail.com>
2015-10-10 20:10:29 +0530

    added Google analytics tracking for course pages

--- portal/student/course.php
+++ portal/student/course.php
@@ -87,6 +87,17 @@
 	<link href="../../css/material/flipper.css" rel="stylesheet">
 	<link href="../../css/material/material-wfont.min.css" rel="stylesheet">
 
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-66359518-1', 'auto');
+		ga('send', 'pageview');
+
+	</script>
+
 	<!--[endif]-->
 </head>
 <body >
