18d3cdfe47e226db956e21e2b3dd52faa717c5bc
tonythomas01 <01tonythomas@gmail.com>
2015-10-11 14:55:06 +0530

    fix size

--- js/main.js
+++ js/main.js
@@ -147,7 +147,7 @@ function main() {
             var owl_home = $("#owl-home");
 
             owl_home.owlCarousel({
-                items : 7, //10 items above 1000px browser width
+                items : 6, //10 items above 1000px browser width
                 itemsDesktop : [1000,3], //5 items between 1000px and 901px
                 itemsDesktopSmall : [900,3], // betweem 900px and 601px
                 itemsTablet: [600,3], //2 items between 600 and 0
